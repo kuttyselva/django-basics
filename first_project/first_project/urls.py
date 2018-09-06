2ffa66db403afd33a65d1ec82268a445a22121a9
kuttyselva <kuttyselva57@gmail.com>
2018-09-07 01:17:32 +0530

    07 sept 2018

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from firstapp import views
+from django.conf.urls import include
 urlpatterns = [
     path('',views.index,name='index'),
+    path('firstapp/',include('firstapp.urls')),
     path('admin/', admin.site.urls),
 
 ]
