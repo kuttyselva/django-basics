65ff05292bc73f19fc57b46d328f8eda222d59ca
kuttyselva <kuttyselva57@gmail.com>
2018-09-23 01:46:23 +0530

    tags

--- protwo/ltemp/basicapp/views.py
+++ protwo/ltemp/basicapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'basicapp/index.html')
+    contextdict={'text':'helloworld','Number':123}
+    return render(request,'basicapp/index.html',context=contextdict)
 def other(request):
     return render(request,'basicapp/other.html')
 def relative(request):
